44ae0f0e8bf9081bb38bfedac9da78b74ff19697
AndreaKvapilova <155962714+AndreaKvapilova@users.noreply.github.com>
2024-05-08 18:31:38 +0200

    Add files via upload

---
+++ elections_scraper.py
@@ -0,0 +1,63 @@
+import threading
+from queue import Queue
+import function
+import csv
+import sys
+
+def main():
+    #control arguments: the number -> arg[0] - script name, arg[1] - territorial unit url, arg[2] - name of csv file
+    if len(sys.argv) != 3: #control number of argument
+        print("The script needs two arguments! The first is the url of the territorial unit, the second is <filename>.csv")
+        return
+    
+    # get arguments to variable from the command line
+    url = sys.argv[1]
+    file_name = sys.argv[2]
+
+    if not file_name.endswith(".csv"): #control format if it is csv file
+        print("Output file input error. Specify the required file name and .csv extension. E. g. prostejov.csv ")
+        return
+    elif not url.startswith("http"): # control format of url address
+        print('Problem with url parameter. Insert like e. g. "https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=12&xobec=506761&xvyber=7103"')
+        return
+
+    #getting the source url
+    response = function.get_html_content(url)
+
+    #create soup
+    list_municipality_names = function.create_bs_findall_list(response, 'td', 'class','overflow_name')
+    code_list = function.create_bs_findall_list(response, 'td', 'class', 'cislo')
+    base_url = "https://volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"
+    print("Preparing beautiful soup, please wait.")
+
+    #creating treads to speed up downloading of municipalities
+    threads = []
+    result_queue = Queue() #for saving results from threads
+
+    for index, municipality_name in enumerate(list_municipality_names):
+        thread = threading.Thread(target=function.process_municipality_soup, args=(municipality_name, code_list[index], base_url, result_queue))
+        thread.start()
+        threads.append(thread)
+    
+    for thread in threads:
+        thread.join()
+
+    #saving the results of individual municipalities from threads to csv
+    try:
+        with open(file_name, mode='a', newline='', encoding='utf-8-sig') as csvfile:
+            while not result_queue.empty():
+                municipality_record = result_queue.get()    
+                values = [item[1] for item in municipality_record] #prepare values for write
+                writer = csv.writer(csvfile)
+
+                if csvfile.tell() == 0: 
+                    headers = [item[0] for item in municipality_record] #prepare headers for write
+                    writer.writerow(headers)
+
+                writer.writerow(values)
+        print(f"File {file_name} was successfully created.")
+    except IOError as e: 
+        print(f"Error writing to file {e}")        
+
+if __name__ == '__main__':
+    main()
---
+++ function.py
@@ -0,0 +1,124 @@
+""" 
+Engeto Online Python Akademie: Projekt3 - Elections Scraper 
+author: Andrea Kvapilová 
+email: a.ndrea@centrum.cz 
+discord: andreakvapilova
+"""
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
+import requests
+import re
+
+def get_html_content(url:str):
+    '''
+    Function to download url and create beautiful soup.
+
+    Args:
+     url(str): string with url
+
+    Returns:
+    object from BeautifulSoup
+    '''
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            #source file splitting with Beautiful Soup
+            soup = bs(response.text, features="html.parser")
+        return soup
+    except requests.RequestException as e:
+        print("Unable to download website. Status code:{response.status_code}")
+        return None
+        
+def create_bs_findall_list(soup, element:str, atribut_name:str, atribut_value:str) -> list:
+    '''
+    The function creates from beautiful soup object a list with searched elements using attribut name.
+
+    Args:
+        soup(bs.element): BeautifulSoup object
+        element (str): HTML element name (e.g. td, tr, a)
+        atribut_name (str): atribut name (e.g. class, headers)
+        atribut_value (str): atribut value (e.g. 'overlow_name', 't3sa1 t3sb1')
+    
+    Returns:
+        list: List of matching elements
+    '''
+    try:
+        list_bs = soup.find_all(element, attrs={atribut_name: atribut_value})
+        return list_bs
+    except ValueError as e:
+        print(f"Error creating a list from Beautiful soup! {e}")
+        return None
+
+def get_text_from_soup(bs_list):
+    '''
+    The function gets the text from the Beautiful soup object.
+
+    Args:
+        bs_list (bs.element): Beautiful soup object
+    
+    Returns:
+        list or single text depending on the number of bs elements in the list
+    '''
+    result = []
+
+    if len(bs_list) == 1:
+        return bs_list[0].get_text()
+    else:
+        result = [element.get_text() for element in bs_list]
+        return result
+
+def create_complete_url(base_url:str, attached_url:str) -> str:
+    '''
+    The function associates a base url address with a relative link
+
+    Args:
+        base_url (str): HTML address
+        attached_url (str): relative link
+
+    Returns:
+        urljoin (str): absolute HTML address
+    '''
+    return urljoin(base_url, attached_url)
+
+def process_municipality_soup(municipality_name, code, base_url, result_queue):
+    '''
+    The function generates from the Bs object search the names of the municipalities,
+     their code and the ralative link to each municipality information for further webscraping
+     (number of voters, envelopes, valid votes and the party names and the muber of votes they
+      received in a given municipality).
+
+    Args:
+        municipality_name: Beautiful soup object
+        code: Beautiful soup objects
+        base_url: str
+        result_queue: queue.Queue
+    
+    Return:
+        municipality_record: list
+    '''
+    municipality_record = []
+    location = municipality_name.get_text()
+    code_location = code.get_text()
+    relative_path = code.find('a')['href']
+    complete_path = create_complete_url(base_url,relative_path)
+    municipality_soup = get_html_content(complete_path)
+    voters = create_bs_findall_list(municipality_soup, 'td', 'headers', 'sa2')
+    text_voters = get_text_from_soup(voters)
+    envelope = create_bs_findall_list(municipality_soup, 'td', 'headers', 'sa3')
+    text_envelope = get_text_from_soup(envelope)
+    valid_votes = create_bs_findall_list(municipality_soup, 'td', 'headers', 'sa6')
+    text_valid = get_text_from_soup(valid_votes)
+    party_name = create_bs_findall_list(municipality_soup, 'td', 'headers', re.compile(r't\d+sa1 t\d+sb2'))
+    text_party_name = get_text_from_soup(party_name)
+    party_value = create_bs_findall_list(municipality_soup, 'td', 'headers', re.compile(r't\d+sa2 t\d+sb3'))
+    text_party_value = get_text_from_soup(party_value)
+
+    #append to municipality record
+    municipality_record.append(['code', code_location])
+    municipality_record.append(['location', location])
+    municipality_record.append(['registred voters', text_voters])
+    municipality_record.append(['issued envelopes', text_envelope])
+    municipality_record.append(['valid votes', text_valid])
+    party = [list(pair) for pair in zip(text_party_name, text_party_value)]
+    municipality_record += party
+    result_queue.put(municipality_record)
